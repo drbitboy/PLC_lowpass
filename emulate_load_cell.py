713dd5909998a3da2426d1c91956f576e914f299
Brian Carcich <briantcarcich@gmail.com>
2022-07-04 12:38:41 -0400

    Fix scaling to match PLC code; shorten startup/reset sleep; add sleep in finally clause so PLC will acquire the last value

--- emulate_load_cell.py
+++ emulate_load_cell.py
@@ -9,22 +9,22 @@ slcdrv=pycomm3.SLCDriver('192.168.1.112/0')
 slcdrv.open()
 
 ssend,sreset,semulate,scancel,sload_cell = 'B3:0/0 B3:0/1 B3:0/3 B3:0/4 N7:1'.split()
-n7_1 = int(round(4096.*loads.pop() / 1000.0))
+n7_1 = int(round(4095.*loads.pop() / 1000.0))
 
 slcdrv.write((sreset,True,))
 try:
     slcdrv.write((semulate,True,))
     slcdrv.write((sload_cell,n7_1,))
 
-    time.sleep(5)
+    time.sleep(2)
     slcdrv.write((sreset,False,))
 
     while loads:
         send_trigger = slcdrv.read(ssend)
         if send_trigger.value:
-            slcdrv.write((ssend,False,))
-            n7_1 = int(round(4096.*loads.pop() / 1000.0))
+            n7_1 = int(round(4095.*loads.pop() / 1000.0))
             slcdrv.write((sload_cell,n7_1,))
+            slcdrv.write((ssend,False,))
         time.sleep(0.02)
 
 except KeyboardInterrupt:
@@ -33,6 +33,7 @@ except:
     import traceback
     traceback.print_exc()
 finally:
+    time.sleep(1.0)
     slcdrv.write((semulate,False,))
     slcdrv.write((scancel,True,))
     time.sleep(1.0)
